a2f5607ac2066c9b8f29ed480d07b01350cad387
Murad Hallak <murad.hallak.99@hotmail.com>
2024-06-19 19:48:39 -0500

    finished styling the projects section

--- resources/script.js
+++ resources/script.js
@@ -11,3 +11,21 @@ document.getElementById('contactForm').addEventListener('submit', function(event
             alert("Failed to send the message.");
         });
 });
+
+const startDate = new Date('2024-06-13');
+  
+    function calculateDaysSince(date) {
+        const now = new Date();
+        const timeDifference = now - date;
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        return daysDifference;
+    }
+
+
+    function updateCounter() {
+        const daysCounterElement = document.getElementById('daysCounter');
+        const daysSince = calculateDaysSince(startDate);
+        daysCounterElement.textContent = daysSince;
+    }
+
+    updateCounter();
